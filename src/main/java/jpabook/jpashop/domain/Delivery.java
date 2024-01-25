26da218bbf8273093eeb4fecda9f2e9faf99511d
Heejun-0219 <heejun0219@gmail.com>
2024-01-25 18:54:01 +0900

    feat: h2 db test

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Delivery {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING) // DB가 중간에 변경되는 순간 다른 값들이 다 변할 수 있다. ORDINARY 사용 절대 금지
+    private DeliveryStatus status;
+}
---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,33 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.autoconfigure.amqp.RabbitConnectionDetails.Address;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member") // 읽기 전용으로 변경
+    private List<Order> orders = new ArrayList<>();
+
+}
