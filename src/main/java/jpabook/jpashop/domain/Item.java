d2fef07a69079782c3fe3c2d2f376717756f3dc4
Heejun-0219 <heejun0219@gmail.com>
2024-01-30 14:15:37 +0900

    feat: 회원 상품 엔티티 개발

--- src/main/java/jpabook/jpashop/domain/Item.java
+++ src/main/java/jpabook/jpashop/domain/Item.java
@@ -11,14 +11,14 @@ import jakarta.persistence.InheritanceType;
 import jakarta.persistence.ManyToMany;
 import java.util.ArrayList;
 import java.util.List;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
-import lombok.Setter;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
 @Getter
-@Setter
+//@Setter -> 세터를 사용하지 않고 비즈니스 로직을 활용해서 프로퍼티를 수정해줘야한다.
 public abstract class Item {
     @Id
     @GeneratedValue
@@ -31,4 +31,27 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items", fetch = FetchType.LAZY)
     private List<Category> categories = new ArrayList<>();
+
+    // 비즈니스 로직
+
+    /**
+     * stock 증가
+     *
+     * @param quantity
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     *
+     * @param quantity
+     */
+    public void removeStock(int quantity) {
+        if (this.stockQuantity - quantity < 0) {
+            throw new NotEnoughStockException("Need more Stock.");
+        }
+        this.stockQuantity -= quantity;
+    }
 }
